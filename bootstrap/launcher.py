4d8e9e76f98b5971c39c7534781fcaf264f5703f
Angel Pavon <agedito@agedito.com>
2026-02-24 09:57:36 +0100

    encapsulate qt app

--- bootstrap/launcher.py
+++ bootstrap/launcher.py
@@ -1,13 +1,9 @@
-import sys
-
-from PySide6.QtWidgets import QApplication
-
 from app.interface.event_bus import EventBus
 from app.track_app.adapter import AppAdapter
 from app.track_app.config import Config as AppConfig
 from app.track_app.main_app import DanceTrackerApp
 from ui.config import Config as UiConfig
-from ui.window.main_window import MainWindow
+from ui.main_app import GraphicApp
 
 
 def launch():
@@ -21,8 +17,5 @@ def launch():
 
     # Graphic user interface
     ui_cfg = UiConfig()
-    qt_app = QApplication(sys.argv)
-    window = MainWindow(ui_cfg, app, adapter)
-
-    events.connect(window)
-    sys.exit(qt_app.exec())
+    ui_app = GraphicApp(app, adapter)
+    ui_app.launch(ui_cfg, events)
